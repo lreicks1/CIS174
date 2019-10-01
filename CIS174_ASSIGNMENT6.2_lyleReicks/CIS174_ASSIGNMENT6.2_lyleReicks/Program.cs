fb6edde00def433c3c51a7fabb8e96a9a70a9f9e
lreicks1 <55216662+lreicks1@users.noreply.github.com>
2019-10-01 17:31:13 -0500

    Add files via upload

---
+++ CIS174_ASSIGNMENT6.2_lyleReicks/CIS174_ASSIGNMENT6.2_lyleReicks/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CIS174_ASSIGNMENT6._2_lyleReicks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ CIS174_ASSIGNMENT6.2_lyleReicks/CIS174_ASSIGNMENT6.2_lyleReicks/Properties/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174_ASSIGNMENT6._2_lyleReicks.Properties
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
