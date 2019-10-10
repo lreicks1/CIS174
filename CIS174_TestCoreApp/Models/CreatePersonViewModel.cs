35a6c6234f855213f50631687944f04b71ae8c02
lreicks1 <55216662+lreicks1@users.noreply.github.com>
2019-10-10 17:34:20 -0500

    upload 10/10

---
+++ CIS174_TestCoreApp/Models/CreatePersonViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CIS174_TestCoreApp.Models
+{
+    public class CreatePersonViewModel
+    {
+        [Required][StringLength(25)] public string FirstName { get; set; }
+
+        [Required][StringLength(25)] public string MiddleName { get; set; }
+
+        [Required][StringLength(25)] public string LastName { get; set; }
+
+        [Required][Range(1, 20)] public int Age { get; set; }
+
+        [Required][EmailAddress] public string Email { get; set; }
+
+        [Required][Compare("Email")] public string ConfirmEmail { get; set; }
+
+        [Required] [Url] [StringLength(7)] public string Website { get; set; }
+
+         public string School { get; set; }
+    }
+}
